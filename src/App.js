d72cf10a9785f01b632e2a0fa8c22b48df2fd266
tamika16 <147372830+tamika16@users.noreply.github.com>
2025-01-08 22:28:06 -0500

    installed bootstrap

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Tamika Englehart and is open-sourced on{" "}
-        <a
-          href="https://github.com/tamika16/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-        .
-      </footer>
+      <div className="contaier">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Tamika Englehart and is open-sourced on{" "}
+          <a
+            href="https://github.com/tamika16/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
