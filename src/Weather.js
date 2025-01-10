437605c30f1617bd0ee6d5be08489774251bbb76
tamika16 <147372830+tamika16@users.noreply.github.com>
2025-01-09 19:08:01 -0500

    added basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -11,27 +11,33 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
       <h1>Pittsburgh</h1>
       <ul>
         <li>Wednesday 10:34</li>
-        <li>Mostly CLoudly</li>
+        <li>Mostly cloudly</li>
       </ul>
       <div className="row">
-        <div className="col-6">
+        <div className="col-7 mt-4">
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/snow_light.png"
             alt="Light snow"
           />
-          -7°C
+          <span className="temperature">-7</span>
+          <span className="unit">°C</span>
         </div>
-        <div className="col-6">
+        <div className="col-5 mt-4">
           <ul>
             <li>Precipitation: 10%</li>
             <li>Humidity: 73%</li>
