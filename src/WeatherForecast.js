1d48145ee03a65541f8b2b00560abc73ef7dad52
tamika16 <147372830+tamika16@users.noreply.github.com>
2025-01-30 19:58:10 -0500

    added basic layout of weather forecast and basic styling

---
+++ src/WeatherForecast.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./WeatherForecast.css";
+
+export default function WeatherForecast() {
+  return (
+    <div className="WeatherForecast">
+      <div className="row">
+        <div className="col">
+          <div className="WeatherForecast-day">Thu</div>
+          <img src="/" />
+          <div className="WeatherForecast-temps">
+            <span className="WeatherForecast-temp-max">19°</span>
+            <span className="WeatherForecast-temp-min">10°</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
